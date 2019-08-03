d8d0c976563326f797e02f34f17a54276a172c1c
4Paradigm <jeremyyoung.1125@gmail.com>
2019-08-03 13:00:29 +0800

    Add RL

---
+++ actorcritic.py
@@ -0,0 +1,26 @@
+import torch 
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+
+class Actor(nn.Module):
+
+    def __init__(self):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(1, 4)
+        self.fc2 = nn.Linear(4, 1)
+
+    def forward(self, x):
+        actv = nn.LeakyReLU(self.fc1(x))
+        return nn.ReLU(self.fc2(actv))
+
+class Critic(nn.Module):
+
+    def __init__(self):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(2, 4)
+        self.fc2 = nn.Linear(4, 1)
+
+    def forward(self, x, a):
+        actv = nn.LeakyReLU(self.fc1(torch.cat([x,a],dim=1)))
+        return nn.Tanh(self.fc2(actv))
