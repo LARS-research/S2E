5e0115079c885cd427d6643279bb2819f61c8bef
4Paradigm <jeremyyoung.1125@gmail.com>
2019-08-27 13:45:01 +0800

    Add CIFAR-100

--- model.py
+++ model.py
@@ -40,62 +40,45 @@ class CNN(nn.Module):
         return x
 
 class CNN_large(nn.Module):
-    def __init__(self, input_channel=3, n_outputs=10, dropout_rate=0.25, top_bn=False):
+    def __init__(self, input_channel=3, n_outputs=10, dropout_rate=0.25, momentum=0.1):
         self.dropout_rate = dropout_rate
-        self.top_bn = top_bn
-        super(CNN, self).__init__()
-        self.c1=nn.Conv2d(input_channel,128,kernel_size=3,stride=1, padding=1)
-        self.c2=nn.Conv2d(128,128,kernel_size=3,stride=1, padding=1)
-        self.c3=nn.Conv2d(128,128,kernel_size=3,stride=1, padding=1)
-        self.c4=nn.Conv2d(128,256,kernel_size=3,stride=1, padding=1)
-        self.c5=nn.Conv2d(256,256,kernel_size=3,stride=1, padding=1)
-        self.c6=nn.Conv2d(256,256,kernel_size=3,stride=1, padding=1)
-        self.c7=nn.Conv2d(256,512,kernel_size=3,stride=1, padding=0)
-        self.c8=nn.Conv2d(512,256,kernel_size=3,stride=1, padding=0)
-        self.c9=nn.Conv2d(256,128,kernel_size=3,stride=1, padding=0)
-        self.l_c1=nn.Linear(128,n_outputs)
-        self.bn1=nn.BatchNorm2d(128,momentum=0.5)
-        self.bn2=nn.BatchNorm2d(128,momentum=0.5)
-        self.bn3=nn.BatchNorm2d(128,momentum=0.5)
-        self.bn4=nn.BatchNorm2d(256,momentum=0.5)
-        self.bn5=nn.BatchNorm2d(256,momentum=0.5)
-        self.bn6=nn.BatchNorm2d(256,momentum=0.5)
-        self.bn7=nn.BatchNorm2d(512,momentum=0.5)
-        self.bn8=nn.BatchNorm2d(256,momentum=0.5)
-        self.bn9=nn.BatchNorm2d(128,momentum=0.5)
+        self.momentum = momentum
+        super(CNN_large, self).__init__()
+        self.c1=nn.Conv2d(input_channel,64,kernel_size=3,stride=1, padding=1)
+        self.c2=nn.Conv2d(64,64,kernel_size=3,stride=1, padding=1)
+        self.c3=nn.Conv2d(64,128,kernel_size=3,stride=1, padding=1)
+        self.c4=nn.Conv2d(128,128,kernel_size=3,stride=1, padding=1)
+        self.c5=nn.Conv2d(128,196,kernel_size=3,stride=1, padding=1)
+        self.c6=nn.Conv2d(196,16,kernel_size=3,stride=1, padding=1)
+        self.l_c1=nn.Linear(256,n_outputs)
+        self.bn1=nn.BatchNorm2d(64,momentum=self.momentum)
+        self.bn2=nn.BatchNorm2d(64,momentum=self.momentum)
+        self.bn3=nn.BatchNorm2d(128,momentum=self.momentum)
+        self.bn4=nn.BatchNorm2d(128,momentum=self.momentum)
+        self.bn5=nn.BatchNorm2d(196,momentum=self.momentum)
+        self.bn6=nn.BatchNorm2d(16,momentum=self.momentum)
 
     def forward(self, x,):
         h=x
         h=self.c1(h)
-        h=F.leaky_relu(call_bn(self.bn1, h), negative_slope=0.01)
+        h=F.relu(call_bn(self.bn1, h))
         h=self.c2(h)
-        h=F.leaky_relu(call_bn(self.bn2, h), negative_slope=0.01)
+        h=F.relu(call_bn(self.bn2, h))
+        h=F.max_pool2d(h, kernel_size=2, stride=2)
+
         h=self.c3(h)
-        h=F.leaky_relu(call_bn(self.bn3, h), negative_slope=0.01)
-        h=F.max_pool2d(h, kernel_size=2, stride=2)
-        h=F.dropout2d(h, p=self.dropout_rate)
-
+        h=F.relu(call_bn(self.bn3, h))
         h=self.c4(h)
-        h=F.leaky_relu(call_bn(self.bn4, h), negative_slope=0.01)
-        h=self.c5(h)
-        h=F.leaky_relu(call_bn(self.bn5, h), negative_slope=0.01)
-        h=self.c6(h)
-        h=F.leaky_relu(call_bn(self.bn6, h), negative_slope=0.01)
+        h=F.relu(call_bn(self.bn4, h))
         h=F.max_pool2d(h, kernel_size=2, stride=2)
-        h=F.dropout2d(h, p=self.dropout_rate)
 
-        h=self.c7(h)
-        h=F.leaky_relu(call_bn(self.bn7, h), negative_slope=0.01)
-        h=self.c8(h)
-        h=F.leaky_relu(call_bn(self.bn8, h), negative_slope=0.01)
-        h=self.c9(h)
-        h=F.leaky_relu(call_bn(self.bn9, h), negative_slope=0.01)
-        h=F.avg_pool2d(h, kernel_size=h.data.shape[2])
+        h=self.c5(h)
+        h=F.relu(call_bn(self.bn5, h))
+        h=self.c6(h)
+        h=F.relu(call_bn(self.bn6, h))
+        h=F.max_pool2d(h, kernel_size=2, stride=2)
 
-        h = h.view(h.size(0), h.size(1))
+        h = h.view(h.size(0), -1)
         logit=self.l_c1(h)
-        if self.top_bn:
-            logit=call_bn(self.bn_c1, logit)
         return logit
-
 
