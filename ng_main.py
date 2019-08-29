ad175300744fd1fea349ee86a2a7d9418536ccd3
jerermyyoung <jeremyyoung.1125@gmail.com>
2019-08-29 10:18:03 +0800

    Fuck CPU

--- ng_main.py
+++ ng_main.py
@@ -18,7 +18,7 @@ from scipy.special import psi
 from numpy.linalg.linalg import inv
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type = float, default = 0.1)
+parser.add_argument('--lr', type = float, default = 0.001)
 parser.add_argument('--result_dir', type = str, help = 'dir to save result txt files', default = 'results/')
 parser.add_argument('--noise_rate', type = float, help = 'corruption rate, should be less than 1', default = 0.2)
 parser.add_argument('--forget_rate', type = float, help = 'forget rate', default = None)
