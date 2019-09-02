dc33c0dca71777554d28441f47d66802456593fe
gpu01 <jeremyyoung.1125@gmail.com>
2019-09-02 14:57:53 +0800

    Accurate fisher estimation

--- random_mnist_sym5.sh
+++ random_mnist_sym5.sh
@@ -2,5 +2,5 @@
 for((i=1;i<=1;i=i+1))
 do
 	# echo "Lap:" $i
-	python random_mnist_main.py --noise_type symmetric --noise_rate 0.5 --num_workers 1 --n_iter 5 --n_samples 20
+	python random_mnist_main.py --noise_type symmetric --noise_rate 0.5 --num_workers 1 --n_iter 5 --n_samples 20 --seed 4
 done
