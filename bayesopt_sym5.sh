eca57e35b6db620d2ae37e9609631fb36a627001
jerermyyoung <jeremyyoung.1125@gmail.com>
2019-09-12 00:45:47 +0800

    Bayesian revise

--- bayesopt_sym5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for((i=1;i<=1;i=i+1))
-do
-	# echo "Lap:" $i
-	python bayesopt_main.py --dataset cifar10 --noise_type symmetric --noise_rate 0.5 --result_dir result1 --num_workers 1 --test_epoch 32 --n_iter 17
-done
