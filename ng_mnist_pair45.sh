cffb7c1f48b3a55ce6e122fb13f12906924be758
jerermyyoung <jeremyyoung.1125@gmail.com>
2019-09-27 11:51:31 +0800

    ICLR

--- ng_mnist_pair45.sh
+++ ng_mnist_pair45.sh
@@ -2,5 +2,5 @@
 for((i=1;i<=1;i=i+1))
 do
 	# echo "Lap:" $i
-	python ng_mnist_main.py --noise_type pairflip --noise_rate 0.45 --num_workers 1 --n_iter 5 --n_samples 20 --delta 20 --fisher_samples 9980 --seed 3
+	python ng_mnist_main.py --noise_type pairflip --noise_rate 0.45 --num_workers 1 --n_iter 5 --n_samples 20 --delta 20 --fisher_samples 9980 --seed 2
 done
--- random_mnist_sym2.sh
+++ random_mnist_sym2.sh
@@ -2,5 +2,5 @@
 for((i=1;i<=1;i=i+1))
 do
 	# echo "Lap:" $i
-	python random_mnist_main.py --noise_type symmetric --noise_rate 0.2 --num_workers 1 --n_iter 5 --n_samples 20 --seed 3
+	python random_mnist_main.py --noise_type symmetric --noise_rate 0.2 --num_workers 1 --n_iter 5 --n_samples 20 --seed 2 
 done
