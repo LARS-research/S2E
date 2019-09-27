70008d64fb07d0f044143ce0a345929027823e1b
gpu01 <jeremyyoung.1125@gmail.com>
2019-09-27 11:41:03 +0800

    ICLR

--- random_sym5.sh
+++ random_sym5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for((i=1;i<=1;i=i+1))
+for((i=1;i<=5;i=i+1))
 do
-	# echo "Lap:" $i
-	python random_main.py --noise_type symmetric --noise_rate 0.5 --num_workers 1 --n_iter 5 --n_samples 10 --result_dir result2/ --seed 4
+	echo "Lap:" $i
+	python random_main.py --noise_type symmetric --noise_rate 0.2 --num_workers 1 --n_iter 5 --n_samples 10 --result_dir result2/ --seed $i
 done
