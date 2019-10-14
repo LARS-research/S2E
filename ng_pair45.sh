a197feb59a2cb8171936e32cc458ae468782a3f8
jerermyyoung <jeremyyoung.1125@gmail.com>
2019-10-14 13:56:06 +0800

    Remove unnecessary codes

--- ng_pair45.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for((i=1;i<=1;i=i+1))
-do
-	# echo "Lap:" $i
-	python ng_main.py --noise_type pairflip --noise_rate 0.45 --num_workers 1 --n_iter 5 --n_samples 10 --delta 500 --fisher_samples 9990 --result_dir result2/ --seed 3
-done
--- ng_sym5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for((i=1;i<=1;i=i+1))
-do
-	# echo "Lap:" $i
-	python ng_main.py --noise_type symmetric --noise_rate 0.5 --num_workers 1 --n_iter 5 --n_samples 10 --delta 500 --fisher_samples 9990 --result_dir result5/ --seed 7
-done
