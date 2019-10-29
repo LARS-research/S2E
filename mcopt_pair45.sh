37cfa8d99f2a9004d69bf3a904208136d5c17192
4Paradigm <jeremyyoung.1125@gmail.com>
2019-10-29 20:37:57 +0800

    Clean code

--- mcopt_pair45.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for((i=1;i<=1;i=i+1))
-do
-	# echo "Lap:" $i
-	python mcopt_main.py --dataset cifar10 --noise_type pairflip --noise_rate 0.45 --result_dir result1 --num_workers 1 > mcopt_pair45.txt 2>&1 &
-done
--- mcopt_sym5.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for((i=1;i<=1;i=i+1))
-do
-	# echo "Lap:" $i
-	python mcopt_main.py --dataset cifar10 --noise_type symmetric --noise_rate 0.5 --result_dir result1 --num_workers 1 > mcopt_sym5.txt 2>&1 &
-done
